9d58e796c0f28f10b7309ca20b9d909bc6c8ea64
PaoloLM <paololm@hotmail.com>
2021-04-28 21:03:58 -0500

    Sesiones - HashHelper

---
+++ Filters/AdminFilters.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Sistema_GGYM.Models;
+
+namespace Sistema_GGYM.Filters
+{
+    public class AdminFilters
+    {
+        public class AutenticadoAttribute : ActionFilterAttribute//AdminFilters
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                base.OnActionExecuting(filterContext);
+
+                if (!SessionHelper.ExistUserInSession())
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                    {
+                        controller = "Login",
+                        action = "Index"
+                    }));
+                }
+            }
+        }
+
+
+
+        // Si estamos logeado ya no podemos acceder a la página de Login
+        public class NoLoginAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                base.OnActionExecuting(filterContext);
+
+                if (SessionHelper.ExistUserInSession())
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                    {
+                        controller = "Home",
+                        action = "Index"
+                    }));
+                }
+            }
+        }
+
+    }
+}
