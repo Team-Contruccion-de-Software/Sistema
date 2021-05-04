2e68762e14ec2f6739b5f25fd5ff2fdb088fbf7a
PaoloLM <paololm@hotmail.com>
2021-05-04 13:09:12 -0500

    Avance Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_GGYM.Models;
+using Sistema_GGYM.Filters;
+
+namespace Sistema_GGYM.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult FormRegistro()
+        {
+            return View();
+        }
+
+
+        public ActionResult FormLogin()
+        {
+            return View();
+        }
+    }
+}
