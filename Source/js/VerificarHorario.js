f89a5b982776584fee53d321ebd3c301376fe758
PaoloLM <paololm@hotmail.com>
2021-05-13 13:01:05 -0500

    Verificar hora inicio y fin del entrenador

---
+++ Source/js/VerificarHorario.js
@@ -0,0 +1,32 @@
+﻿var horainicio;
+var horafin;
+
+var horaentera;
+var horainicioentera;
+var horafinentera;
+
+function validarhora(inicio, fin) {
+
+    var fecha = new Date();
+    var hora = fecha.getHours() + ':' + fecha.getMinutes();
+    horaentera = parseInt(fecha.getHours() + "" + fecha.getMinutes());
+
+    horainicioentera = parseInt(inicio);
+    horainicio = String(inicio);
+    horainicio = horainicio.substring(0, 2) + ":" + horainicio.substring(2, 4);
+
+    horafinentera = parseInt(fin);
+    horafin = String(fin);
+    horafin = horafin.substring(0, 2) + ":" + horafin.substring(2, 4);
+
+
+    if (horainicioentera > horaentera || horafinentera < horaentera ) {
+        alert("Aun no empieza");
+    }
+
+    if (horainicioentera < horaentera && horafinentera > horaentera) {       
+        alert("empezo");
+         //MOSTRAR MODAL DE CONFIRMACION
+    }
+
+}
