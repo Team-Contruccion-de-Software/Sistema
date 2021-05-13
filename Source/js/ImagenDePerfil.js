cf85737b033bb86975ddadfb8c6b8fcf0ed102ed
PaoloLM <paololm@hotmail.com>
2021-05-13 11:15:01 -0500

    Mejoras en la UI y eliminacion de js no utilizados

---
+++ Source/js/ImagenDePerfil.js
@@ -0,0 +1,41 @@
+﻿var imagen = document.getElementById("ImagenPefil");
+var usuario = document.getElementById("usuario").innerText;
+var nombrehtml = location.pathname.split("/").slice(-2);
+var idllamada = location.pathname.split("/").slice(-1);
+
+if (usuario.includes("Paolo")) {
+    imagen.src = "/Source/images/Fotos/Paolo.jpg";
+
+} else if (usuario.includes("Bayron")) {
+    imagen.src = "/Source/images/Fotos/Bayron.jpg";
+
+} else if (usuario.includes("Daniel")) {
+    imagen.src = "/Source/images/Fotos/danielo.jpg";
+
+} else if (usuario.includes("Alvaro")) {
+    imagen.src = "/Source/images/Fotos/alvaro.jpg";
+
+} else if (usuario.includes("CAMILA")) {
+    imagen.src = "/Source/images/Fotos/camila.jpg";
+
+} else if (usuario.includes("Piero")) {
+    imagen.src = "/Source/images/Fotos/piero.jpg";
+
+} else {
+    imagen.src = "/Source/User/images/users/user-icon.jpg";
+}
+
+//GENERANDO EL NOMBRE DE LA LLAMADA
+
+if (nombrehtml.includes("Videollamada")) {
+
+    var identificadorUnico = eliminarTildes(usuario);
+    var nombrellamada = document.getElementById("username-input");
+    nombrellamada.value = "GGYM" + identificadorUnico + idllamada + "GGYM";
+}
+
+function eliminarTildes(cadena) {
+    cadena = cadena.replace(" ", "").toLowerCase();
+    const acentos = { 'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U' };
+    return cadena.split('').map(letra => acentos[letra] || letra).join('').toString();
+}
---
+++ Source/js/Indices.js
@@ -0,0 +1,87 @@
+﻿var nombrehtml = location.pathname.split("/").slice(-1);
+//console.log(nombrehtml);
+
+var titulo = document.getElementById('Titulo');
+var indice1 = document.getElementById('Indice1');
+var indice2 = document.getElementById('Indice2');
+var indice3 = document.getElementById('Indice3');
+var indice4 = document.getElementById('Indice4');
+var indice5 = document.getElementById('Indice5');
+var indice6 = document.getElementById('Indice6');
+
+//Estilo para el default del usuario
+if (nombrehtml == "" || nombrehtml.includes("Usuario")) {
+    indice1.className = 'waves-effect active';
+    titulo.innerHTML = 'Pagina Principal';
+    for (var i = 1; i < 8; i++) {
+        if (i != 1) {
+            ("indice" + i).className = 'waves-effect';
+        }
+    }
+}
+
+//Estilo para el Indice del usuario
+if (nombrehtml.includes("Index")) {
+    indice1.className = 'waves-effect active';
+    titulo.innerHTML = 'Pagina Principal';
+    for (var i = 1; i < 8; i++) {
+        if (i != 1) {
+            ("indice" + i).className = 'waves-effect';
+        }        
+    }
+}
+
+//Estilo para el Calendario del usuario
+if (nombrehtml.includes("Calendario")) {
+    indice2.className = 'waves-effect active';
+    titulo.innerHTML = 'Calendario';
+    for (var i = 1; i < 8; i++) {
+        if (i != 2) {
+            ("indice" + i).className = 'waves-effect';
+        }
+    }
+}
+
+//Estilo para el CoachHorario del usuario
+if (nombrehtml.includes("CoachHorario")) {
+    indice3.className = 'waves-effect active';
+    titulo.innerHTML = 'Horarios';
+    for (var i = 1; i < 8; i++) {
+        if (i != 3) {
+            ("indice" + i).className = 'waves-effect';
+        }
+    }
+}
+
+//Estilo para el Horario del usuario
+if (nombrehtml.includes("Horario")) {
+    indice4.className = 'waves-effect active';
+    titulo.innerHTML = 'Horarios';
+    for (var i = 1; i < 8; i++) {
+        if (i != 4) {
+            ("indice" + i).className = 'waves-effect';
+        }
+    }
+}
+
+//Estilo para el Recomendaciones del usuario
+if (nombrehtml.includes("Recomendaciones")) {
+    indice5.className = 'waves-effect active';
+    titulo.innerHTML = 'Recomendaciones';
+    for (var i = 1; i < 8; i++) {
+        if (i != 5) {
+            ("indice" + i).className = 'waves-effect';
+        }
+    }
+}
+
+//Estilo para el Seguimiento del usuario
+if (nombrehtml.includes("Seguimiento")) {
+    indice6.className = 'waves-effect active';
+    titulo.innerHTML = 'Seguimiento';
+    for (var i = 1; i < 8; i++) {
+        if (i != 6) {
+            ("indice" + i).className = 'waves-effect';
+        }
+    }
+}
