1225eeab053ca893fd2fe0cf5ea03086165237d0
PaoloLM <paololm@hotmail.com>
2021-05-15 13:55:52 -0500

    Pantalla de proximamente

---
+++ Controllers/SeguimientoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_GGYM.Controllers
+{
+    public class SeguimientoController : Controller
+    {
+        // GET: Seguimiento
+        public ActionResult Seguimiento()
+        {
+            return View();
+        }
+    }
+}
