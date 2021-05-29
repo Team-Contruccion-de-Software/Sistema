f9fa2b4d017c96fe50d2775b5fbbc2d1543d7730
bayuron sanchez <71676171+BayronAndresSR@users.noreply.github.com>
2021-05-28 19:48:42 -0500

    Ejercicios-Dietas-Traduccion

--- Source/js/Dietas.js
+++ Source/js/Dietas.js
@@ -1,27 +1,27 @@
-﻿//Creacion de arreglos para almacenar informacion de los ejercicios
+﻿//Creacion de arreglos para almacenar informacion de las dietas
 
-//Titulo de los ejercicios
-let titulodescripcion = ["Plie Squat Scoop Up"
-    , "Side Lunge Front Raise"
-    , "Remo vertical de peso muerto"
-    , "Curvas laterales de sumo"
-    , "Lunge Punch"
-    , "Press de sentadillas dividido"
-    , "Curl en cuclillas"
-    , "Plank Bird Dog"
-    , "Levantamiento de patada lateral con estocada de reverencia"
+//Titulo de las Dietas
+let titulodescripcion = ["Dieta Mediterranea"
+    , "Dieta Dash"
+    , "Dieta Flexitariana"
+    , "Dieta Keto"
+    , "Dieta Hipocalórica"
+    , "Dieta por Puntos"
+    , "Dieta Paleo"
+    , "Dieta Proteica"
+    , "Dieta Alcalina"
 ];
 
-//Descripcion de los ejercicios
-let descripcion = ["Plie squat scoop up ejercicio guía con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de elevación frontal de estocada lateral con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de remo vertical de peso muerto con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de curvas laterales de sumo con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de Lunge punch con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud"
-    , "Guía de ejercicios de press de cuclillas dividido con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de sentadillas curl con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de perro pájaro de tabla con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de levantamiento de patada lateral y estocada de reverencia con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
+//Descripcion de las Dietas
+let descripcion = [" Se basa en los ingredientes propios de la agricultura local de los países con clima mediterráneo, fundamentalmente España e Italia. Se resume en reducir el consumo de carnes e hidratos de carbono en beneficio de más alimentos vegetales y grasas monoinsaturadas."
+    , "La dieta DASH incluye menús con muchos vegetales, frutas y productos lácteos con bajo contenido de grasa, así como cereales integrales, pescado, carne de ave y frutos secos. Ofrece porciones limitadas de carnes rojas, dulces y bebidas azucaradas."
+    , "Suelen contener mucho vegetal, frutas, verdura, cereales, legumbres, frutos secos y semillas, e incluyen cantidades moderadas de carne y variables de pescado y lácteos. La dieta flexitariana, más que restringir, estimula la variedad puesto que no excluye ningún alimento específico."
+    , "En este tipo de dieta, se realizan cinco comidas al día y no se evita ningún grupo de alimentos, excepto aquellos como el azúcar y las grasas saturadas. Estos parámetros son aptos en una dieta para diabéticos, puesto que, además de repartir las comidas a lo largo del día,  limitan la cantidad de grasa, azúcares y sal que ingerimos, y permite consumir una gran variedad de frutas y verduras."
+    , "Cuenta con una tabla de puntos que los clasifica según sus características. El objetivo de este plan de alimentación es no sobrepasar los puntos indicados al cabo del día. En esta dieta no hay alimentos prohibidos, pero siempre hay que evitar aquellos como bollería o embutidos grasos, porque su alta puntuación puede hacer que alcancemos la totalidad de los puntos, o los sobrepasemos, con sólo probarlos."
+    , "Este tipo de dieta elimina los alimentos procesados y evita algunos grupos como cereales o lácteos, y basa la alimentación en carnes, pescados, frutas y verduras, que proporcionan un alto grado de energía. Aunque puede ser una buena opción, hay que encontrar el equilibrio en el consumo de estos productos, ya que un consumo excesivo de carne también puede ser perjudicial."
+    , "En la dieta proteica, se limita la alimentación a aquellos alimentos que son altos en proteínas, como la carne, el pescado y los derivados lácteos. Este tipo de dieta, que llega a excluir grupos de alimentos tan fundamentales como las frutas y las verduras, puede llegar a ocasionar graves problemas de salud. Algunas de las carencias más graves de esta dieta son la falta de hidratos de carbono y de fibra, imprescindibles para el buen funcionamiento de nuestro cuerpo."
+    , "Este tipo de dieta surge en los últimos años como una forma de depurar nuestro organismo, y se basa en el consumo exclusivo de líquidos durante, al menos, un día completo a la semana. Un consumo exclusivo de líquido no sólo no aporta la cantidad de energía necesaria para el funcionamiento del organismo, sino que puede provocar desequilibrios en componentes tan necesarios como el calcio, el potasio o el sodio."
+    , "Conocida como la dieta del pH y promocionada como la dieta “anticáncer”, basándose en el consumo de alimentos que tienen supuestos efectos sobre la acidez de los fluidos de nuestro organismo. El punto a favor de esta dieta es que aboga por alimentos como cereales, frutas, verduras y legumbres, pero con una restricción tan amplia en el resto de alimentos que no es una opción saludable."
 ];
 
 //Creando el estilo
@@ -35,8 +35,8 @@ var galeria1 = document.getElementById("galeria1");
 //Agregando div con los estilos dentro
 for (var i = 1; i < descripcion.length + 1; i++) {
 
-    var imagen = '<a href="/Source/images/Ejercicios/ejercicio' + i + '.gif" class="image-popup" title="Screenshot-' + i + '">';
-    var imagen2 = '<img class="img-responsive img-circle" src="/Source/images/Ejercicios/ejercicio' + i + '.gif" class="thumb-img" alt="work-thumbnail"></a>';
+    var imagen = '<a href="/Source/images/dietas/dieta' + i + '.jpg" class="image-popup" title="Screenshot-' + i + '">';
+    var imagen2 = '<img class="img-responsive img-circle" src="/Source/images/dietas/dieta' + i + '.jpg" class="thumb-img" alt="work-thumbnail"></a>';
     var titulo = '<h4 id="Titulo' + i + '">' + titulodescripcion[i - 1] + '</h4>';
 
     galeria1.innerHTML += parte1 + imagen + imagen2 + titulo + p + descripcion[i - 1] + '</p>' + '</div>' + '</div>';
--- Source/js/Ejercicios.js
+++ Source/js/Ejercicios.js
@@ -1,27 +1,27 @@
 ﻿//Creacion de arreglos para almacenar informacion de los ejercicios
 
 //Titulo de los ejercicios
-let titulodescripcion = ["Plie Squat Scoop Up"
-    , "Side Lunge Front Raise"
+let titulodescripcion = ["Sentadilla Plié "
+    , "Elevacion frontal con estocada lateral"
     , "Remo vertical de peso muerto"
     , "Curvas laterales de sumo"
-    , "Lunge Punch"
+    , "Puñetazo de estocada"
     , "Press de sentadillas dividido"
     , "Curl en cuclillas"
-    , "Plank Bird Dog"
+    , "El Bird Dog"
     , "Levantamiento de patada lateral con estocada de reverencia"
 ];
 
 //Descripcion de los ejercicios
-let descripcion = ["Plie squat scoop up ejercicio guía con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de elevación frontal de estocada lateral con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de remo vertical de peso muerto con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de curvas laterales de sumo con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de Lunge punch con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud"
-    , "Guía de ejercicios de press de cuclillas dividido con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de sentadillas curl con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de perro pájaro de tabla con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
-    , "Guía de ejercicios de levantamiento de patada lateral y estocada de reverencia con instrucciones, demostración, calorías quemadas y músculos trabajados. Aprenda la forma adecuada, descubra todos los beneficios para la salud y elija un entrenamiento."
+let descripcion = ["La sentadilla plié es un movimiento compuesto que se dirige a tus glúteos, muslos, piernas, caderas, pecho y brazos. Este es un ejercicio de cuerpo completo que estimula el metabolismo, fortalece todo el cuerpo y mejora su nivel de condición física."
+    , "Da un paso hacia un lado con la pierna derecha, haz una estocada y levanta las mancuernas hasta que tus brazos queden ligeramente por encima de los paralelos al suelo."
+    , "El remo vertical es un ejercicio de peso libre, que generalmente se realiza con mancuernas o con barra. Es un ejercicio simple, pero requiere una técnica perfecta para conseguir los mejores resultados y evitar lesiones."
+    , "En esta variante de la sentadilla se modifica la distancia de separación entre las piernas y el ángulo de apertura de los pies; así se consigue una mayor participación de los aductores y el glúteo mayor en la extensión de la cadera en su ejecución."
+    , "Manteniendo la espalda erguida, da un paso hacia adelante (alrededor de 1,5 veces un paso hacia adelante normal). Baje su cuerpo hacia la estocada hasta que la pierna delantera alcance un ángulo de 90 grados. Empuje hacia arriba a través de la pierna delantera y lleve su cuerpo de regreso a la posición inicial."
+    , "En este ejercicio se entrena los Cuádriceps, Glúteos, Hombros, Brazos, Músculos abdominales, Todo el cuerpo."
+    , "Empiece a pararse con los pies separados a la altura de los hombros y las pesas a cada lado de usted. Realice un rizo, luego gire los hombros y presione para encontrar los pesos sobre su cabeza."
+    , "El bird-dog es un ejercicio de cuerpo completo que aprieta su núcleo y fortalece sus glúteos, isquiotibiales, cuádriceps, caderas, hombros y brazos. Este ejercicio ayuda a prevenir el dolor lumbar y mejora su postura y equilibrio."
+    , "Involucre su núcleo, mire hacia adelante, abra el pecho y mantenga la espalda recta. Mientras se lanza, mantenga la rodilla delantera sobre el tobillo y mantenga los dedos de los pies apuntando en la misma dirección que las rodillas. Exhale mientras se pone de pie y, mientras patea hacia un lado y levanta la mancuerna, mantenga la espalda alineada."
 ];
 
 //Creando el estilo
