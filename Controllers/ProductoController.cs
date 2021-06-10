7f66a94d3c1e890efe1b5754f92a43a3e3384908
Daniel <jordanloja20@gmail.com>
2021-06-10 15:05:10 -0500

    Listar y agregar producto

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static Sistema_GGYM.Filters.AdminFilters;
+using Sistema_GGYM.Models.Base_De_Datos;
+using Sistema_GGYM.Models;
+
+namespace Sistema_GGYM.Controllers
+{
+    [Autenticado]
+    public class ProductoController : Controller
+    {
+        PRODUCTO producto = new PRODUCTO();
+        CATEGORIA_PRODUCTO categoria_producto = new CATEGORIA_PRODUCTO();
+
+        public ActionResult Index()
+        {
+            return View(producto.ListarTodo());
+        }
+
+        public ActionResult Guardar(PRODUCTO producto)
+        {
+            if (ModelState.IsValid)
+            {
+                producto.RegistrarProducto();
+                return Redirect("~/Producto");
+            }
+            else
+            {
+                return View("~/Producto");
+            }
+        }
+    }
+}
--- Models/Base De Datos/PRODUCTO.cs
+++ Models/Base De Datos/PRODUCTO.cs
@@ -4,7 +4,9 @@ namespace Sistema_GGYM.Models.Base_De_Datos
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("PRODUCTO")]
     public partial class PRODUCTO
@@ -38,5 +40,68 @@ namespace Sistema_GGYM.Models.Base_De_Datos
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<REPORTES> REPORTES { get; set; }
+
+        public List<PRODUCTO> ListarTodo()
+        {
+            var productos = new List<PRODUCTO>();
+
+            try
+            {
+                using (var db = new ModeloGGYM())
+                {
+                    productos = db.PRODUCTO.Include("CATEGORIA_PRODUCTO").ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            return productos;
+        }
+
+        public void RegistrarProducto()
+        {
+            try
+            {
+                using (var db = new ModeloGGYM())
+                {
+                    if (this.ID_PRODUCTO > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public PRODUCTO ObtenerProducto(int id)
+        {
+            var producto = new PRODUCTO();
+
+            try
+            {
+                using (var db = new ModeloGGYM())
+                {
+                    producto = db.PRODUCTO.Include("CATEGORIA_PRODUCTO")
+                        .Where(x => x.ID_PRODUCTO == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return producto;
+        }
     }
 }
