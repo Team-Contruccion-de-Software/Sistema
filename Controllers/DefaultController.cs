01164e678d2108ee1088b93817c821668e17f8de
PaoloLM <paololm@hotmail.com>
2021-06-15 12:55:52 -0500

    Catalogo de productos v1

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -5,14 +5,22 @@ using System.Web;
 using System.Web.Mvc;
 using Sistema_GGYM.Models;
 using Sistema_GGYM.Filters;
+using Sistema_GGYM.Models.Base_De_Datos;
 
 namespace Sistema_GGYM.Controllers
 {
     public class DefaultController : Controller
-    {        
+    {
+        PRODUCTO producto = new PRODUCTO();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Productos()
+        {
+            return View(producto.ListarTodo());
         }
     }
 }
