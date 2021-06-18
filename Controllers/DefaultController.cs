a016cf6e47ac0d79d3a5b547879fcbb0c7860118
PaoloLM <paololm@hotmail.com>
2021-06-18 17:00:52 -0500

    Logo, contacto y productos v2

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using Sistema_GGYM.Models;
 using Sistema_GGYM.Filters;
 using Sistema_GGYM.Models.Base_De_Datos;
+using System.Net.Mail;
+using System.Net;
 
 namespace Sistema_GGYM.Controllers
 {
@@ -31,6 +33,33 @@ namespace Sistema_GGYM.Controllers
         public ActionResult Contacto()
         {
             return View();
+        }
+
+        public ActionResult Enviar(string Email, string name, string Telefono, DateTime Fecha, string Mensaje)
+        {
+            string body =
+                "<body>" +
+                    "<h1>Mensaje de " + name + "</h1>" +
+                    "<h5>Email emisor: " + Email + " , Telefono: " + Telefono + "</h5>" +
+                    "<spam>Contenido del mensaje: " + Mensaje + "</spam>" +
+                    "<br/><br/><spam>Enviado el dia: " + Fecha.ToString("dd/MM/yyyy") + "</spam>" +
+                "<body>";
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("hakunaggym@gmail.com", "Computo.123"); //CREDENCIALES
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+
+            MailMessage correo = new MailMessage();
+            correo.From = new MailAddress("hakunaggym@gmail.com", "Sistema GGYM"); //ORIGEN Y NOMBRE
+            correo.To.Add(new MailAddress("hakunaggym@gmail.com", "Sistema GGYM")); //DESTINO Y NOMBRE
+            correo.Subject = "Mensajes del contacto"; //ASUNTO
+            correo.IsBodyHtml = true;
+            correo.Body = body;
+
+            smtp.Send(correo);
+            return Redirect("~/Default/Contacto");
         }
     }
 }
--- Source/User/productos/js/main.js
+++ Source/User/productos/js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         dist: -80,
         shift: 5,
         padding: 5,
-        numVisible: 3,
+        numVisible: 5,
         indicators: true,
         noWrap: false
     });
